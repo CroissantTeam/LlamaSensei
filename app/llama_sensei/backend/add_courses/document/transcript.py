ec60a53879c31d30677086df1dc403f56c69eacf
huy.ng <nguyenduchuy1711@gmail.com>
2024-08-10 21:14:54 +0700

    config with project video

--- app/llama_sensei/backend/add_courses/document/transcript.py
+++ app/llama_sensei/backend/add_courses/document/transcript.py
@@ -2,21 +2,23 @@
 
 import os
 
+import httpx
 from deepgram import DeepgramClient, FileSource, PrerecordedOptions
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Path to the audio file
-AUDIO_FILE = "spacewalk.mp3"
+AUDIO_FILE = ""
 
 API_KEY = os.getenv("DG_API_KEY")
+print("API_KEY", API_KEY)
 
 
 def main():
     try:
         # STEP 1 Create a Deepgram client using the API key
-        dg = DeepgramClient(API_KEY)
+        deepgram_client = DeepgramClient(API_KEY)
 
         with open(AUDIO_FILE, "rb") as file:
             buffer_data = file.read()
@@ -27,15 +29,17 @@ def main():
 
         # STEP 2: Configure Deepgram options for audio analysis
         options = PrerecordedOptions(
-            model="nova-2",
-            smart_format=True,
+            model="nova-2", smart_format=True, sample_rate=48000
         )
 
         # STEP 3: Call the transcribe_file method
-        r = dg.listen.prerecorded.v("1").transcribe_file(payload, options)
+        r = deepgram_client.listen.prerecorded.v("1").transcribe_file(
+            payload, options, timeout=httpx.Timeout(900.0, connect=10.0)
+        )
 
         # STEP 4: Print the response
-        print(r.to_json(indent=4))
+        with open("stanford_cs229_l1.json", "w") as f:
+            f.write(r.to_json(indent=4))
 
     except Exception as e:
         print(f"Exception: {e}")
--- app/llama_sensei/backend/add_courses/document/vad.py
+++ app/llama_sensei/backend/add_courses/document/vad.py
@@ -1,5 +1,37 @@
+import json
+
 from silero_vad import get_speech_timestamps, load_silero_vad, read_audio
 
 model = load_silero_vad()
-wav = read_audio('path_to_audio_file')
-speech_timestamps = get_speech_timestamps(wav, model)
+wav = read_audio("")
+speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True)
+print(len(speech_timestamps))
+
+# with open("test.json", "r") as f:
+#     content = f.read()
+# speech_timestamps = json.loads(content)
+
+merge_thresh = 0.5  # seconds
+duration_thresh = 0.5  # seconds
+merged_timestamps = []
+for i in range(1, len(speech_timestamps)):
+    time_gap = speech_timestamps[i]["start"] - speech_timestamps[i - 1]["end"]
+    if time_gap <= merge_thresh:
+        speech_timestamps[i]["start"] = speech_timestamps[i - 1]["start"]
+    else:
+        segment_duration = (
+            speech_timestamps[i - 1]["end"] - speech_timestamps[i - 1]["end"]
+        )
+        if segment_duration >= duration_thresh:
+            merged_timestamps.append(speech_timestamps[i - 1])
+
+# result = json.dumps(speech_timestamps, indent=4)
+# with open("test.json", "w") as f:
+#     f.write(result)
+
+result = json.dumps(merged_timestamps, indent=4)
+with open("test_merge.json", "w") as f:
+    f.write(result)
+
+print("speech_timestamps", len(speech_timestamps))
+print("merged_timestamps", len(merged_timestamps))
