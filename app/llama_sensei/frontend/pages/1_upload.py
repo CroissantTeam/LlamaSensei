0389114496c688e71c81305a9a8b3a9c59bc2b25
micache <huuhoangabcd@gmail.com>
2024-08-22 16:54:54 +0000

    finish erase db button

--- app/llama_sensei/frontend/pages/1_upload.py
+++ app/llama_sensei/frontend/pages/1_upload.py
@@ -11,6 +11,8 @@ from llama_sensei.backend.add_courses.vectordb.document_processor import (
 from llama_sensei.backend.add_courses.yt_api.audio import YouTubeAudioDownloader
 from llama_sensei.backend.add_courses.yt_api.playlist import PlaylistVideosFetcher
 
+import shutil
+
 if 'list_name' not in st.session_state:
     client = chromadb.PersistentClient(path="data/chroma_db")
     st.session_state.list_name = [x.name for x in client.list_collections()]
@@ -37,6 +39,8 @@ course_name = st.selectbox(
 if st.button("Add new"):
     create_course()
 
+erase_db = st.checkbox("Erase all data currently in course database")
+
 st.write("Paste the youtube link of the course:")
 url = st.text_input(
     "Example: https://www.youtube.com/watch?list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU"
@@ -61,6 +65,8 @@ def upload():
         print("transcript success")
 
         proc = DocumentProcessor(course_name, search_only=False)
+        if erase_db:
+            proc.erase_all_data()
         folder_path = f"data/transcript/{course_name}/"
         for video_id in os.listdir(folder_path):
             proc.process_document(
@@ -72,5 +78,8 @@ def upload():
         st.rerun()
     st.success("Completed upload")
 
+    # Clean up
+    shutil.rmtree(os.path.join("data", course_name))
+    shutil.rmtree(os.path.join("data/transcript", course_name))
 
 st.button("Upload", on_click=upload)
