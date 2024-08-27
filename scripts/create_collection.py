1cf1eaae4ceac358edc1664472b07504d5e71190
micache <huuhoangabcd@gmail.com>
2024-08-27 12:07:58 +0000

    update create_collection

--- scripts/create_collection.py
+++ scripts/create_collection.py
@@ -1,9 +1,20 @@
+import glob
+import os
+
 from llama_sensei.backend.add_courses.vectordb.document_processor import (
     DocumentProcessor,
 )
-
-proc = DocumentProcessor('cs229_stanford', search_only=False)
-proc.process_document(
-    path="data/stanford_cs229_l1.json", metadata={'video_id': "jGwO_UgTS7I"}
+from llama_sensei.backend.add_courses.vectordb.vector_db_operations import (
+    VectorDBOperations,
 )
+
+course_name = "cs224n_stanford"
+transcript_folder = f"/shared/final/{course_name}/transcript/"
+transcript_files = glob.glob(os.path.join(transcript_folder, "*.json"))
+ids = [os.path.basename(name)[:-5] for name in transcript_files]
+print(transcript_files, ids)
+
+proc = DocumentProcessor(course_name, search_only=False)
+for path, video_id in zip(transcript_files, ids):
+    proc.process_document(path=path, metadata={'video_id': video_id})
 # print(proc.vector_db.client.list_collections())
